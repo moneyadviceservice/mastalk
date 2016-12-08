8ed6ecc964f2099b18efee0b80fea8900a9655bc
Tomas D'Stefano <tomas_stefano@successoft.com>
2016-12-08 12:30:16 +0000

    Organizing the gem, class names and files
    * IMHO the document class should have your own place inside of the
    gem since it is a gem convention

---
+++ lib/mastalk/document.rb
@@ -0,0 +1,39 @@
+require 'kramdown'
+require 'htmlentities'
+require 'mastalk/extensions'
+
+module Mastalk
+  # Document class to preprocess
+  # mastalk specific syntax
+  class Document
+    include Mastalk::Extensions
+
+    attr_reader :source
+
+    def initialize(source)
+      @source = source.dup
+    end
+
+    def to_html(options = {})
+      options[:auto_id] = options[:auto_id].nil? ? true : options[:auto_id]
+      ::HTMLEntities.new.decode( kramdown(options).to_html() )
+    end
+
+    private
+
+    def kramdown(options = {})
+      options[:auto_id] = options[:auto_id].nil? ? true : options[:auto_id]
+      Kramdown::Document.new(preprocess(source), :auto_ids => options[:auto_id])
+    end
+
+    def preprocess(source)
+      extensions.map do |regex, block|
+        if source.match(regex)
+          source.sub!(regex, block.call(Regexp.last_match.captures.first).strip)
+        end
+      end
+      preprocess(source) if extensions.any? {|regex, _| source.match(regex)}
+      source
+    end
+  end
+end
